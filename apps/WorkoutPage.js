e98583d9d773e89c8f4596ae503a10757a917578
Victor-starr <v94625230@gmail.com>
2025-02-05 15:55:27 +0200

    fixing the workout page

--- apps/WorkoutPage.js
+++ apps/WorkoutPage.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, TextInput, ScrollView,SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, ScrollView, SafeAreaView, TextInput } from 'react-native';
 import { Calendar } from 'react-native-calendars';
-import * as CalendarAPI from 'expo-calendar';
 
 function WorkoutPage() {
   const [selectedDate, setSelectedDate] = useState('');
   const [workouts, setWorkouts] = useState({});
   const [showForm, setShowForm] = useState(false);
   const [form, setForm] = useState({
-    title: '',
     machine: '',
     sets: '',
     reps: '',
@@ -16,19 +14,9 @@ function WorkoutPage() {
     finalWeight: ''
   });
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await CalendarAPI.requestCalendarPermissionsAsync();
-      if (status === 'granted') {
-        const calendars = await CalendarAPI.getCalendarsAsync(CalendarAPI.EntityTypes.EVENT);
-        console.log('Here are all your calendars:');
-        console.log({ calendars });
-      }
-    })();
-  }, []);
-
   const handleDateSelect = (day) => {
     setSelectedDate(day.dateString);
+    setShowForm(false); // Hide the form when switching dates
   };
 
   const handleInputChange = (name, value) => {
@@ -38,11 +26,10 @@ function WorkoutPage() {
   const handleSubmit = () => {
     const updatedWorkouts = {
       ...workouts,
-      [selectedDate]: [...(workouts[selectedDate] || []), form]
+      [selectedDate]: [...(workouts[selectedDate] || []), form],
     };
     setWorkouts(updatedWorkouts);
     setForm({
-      title: '',
       machine: '',
       sets: '',
       reps: '',
@@ -52,94 +39,108 @@ function WorkoutPage() {
     setShowForm(false);
   };
 
+  const getMarkedDates = () => {
+    const markedDates = {};
+    Object.keys(workouts).forEach((date) => {
+      markedDates[date] = { marked: true };
+    });
+    if (selectedDate) {
+      markedDates[selectedDate] = { selected: true, selectedColor: '#38BDF8' };
+    }
+    return markedDates;
+  };
+
   return (
-    <SafeAreaView style={{ flex: 1,backgroundColor: '#0F172A', }}>
-    <ScrollView style={styles.container}>
-      <Text style={styles.header}>Workout Page</Text>
-      <Calendar
-        onDayPress={handleDateSelect}
-        markedDates={{
-          [selectedDate]: { selected: true, marked: true, selectedColor: '#38BDF8' }
-        }}
-        theme={{
-          calendarBackground: '#1E293B',
-          textSectionTitleColor: '#94A3B8',
-          selectedDayBackgroundColor: '#38BDF8',
-          selectedDayTextColor: '#ffffff',
-          todayTextColor: '#38BDF8',
-          dayTextColor: '#ffffff',
-          textDisabledColor: '#3B4252',
-          monthTextColor: '#ffffff',
-          arrowColor: '#38BDF8',
-          indicatorColor: '#38BDF8',
-        }}
-      />
-      <Button title="Add Workout" onPress={() => setShowForm(!showForm)} />
-      {showForm && (
-        <View style={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeholder="Title (e.g., Upper Body, Leg Day)"
-            placeholderTextColor="#A0AEC0"
-            value={form.title}
-            onChangeText={(text) => handleInputChange('title', text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Machine (e.g., Leg Press, Bench Press)"
-            placeholderTextColor="#A0AEC0"
-            value={form.machine}
-            onChangeText={(text) => handleInputChange('machine', text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Sets"
-            placeholderTextColor="#A0AEC0"
-            value={form.sets}
-            onChangeText={(text) => handleInputChange('sets', text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Reps"
-            placeholderTextColor="#A0AEC0"
-            value={form.reps}
-            onChangeText={(text) => handleInputChange('reps', text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Warm Up Weight"
-            placeholderTextColor="#A0AEC0"
-            value={form.warmUpWeight}
-            onChangeText={(text) => handleInputChange('warmUpWeight', text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Final Weight"
-            placeholderTextColor="#A0AEC0"
-            value={form.finalWeight}
-            onChangeText={(text) => handleInputChange('finalWeight', text)}
-          />
-          <Button title="Save Workout" onPress={handleSubmit} />
-        </View>
-      )}
-      <View style={styles.workoutList}>
-        {workouts[selectedDate] && workouts[selectedDate].map((workout, index) => (
-          <View key={index} style={styles.workoutItem}>
-            <Text style={styles.workoutText}>Title: {workout.title}</Text>
-            <Text style={styles.workoutText}>Machine: {workout.machine}</Text>
-            <Text style={styles.workoutText}>Sets: {workout.sets}</Text>
-            <Text style={styles.workoutText}>Reps: {workout.reps}</Text>
-            <Text style={styles.workoutText}>Warm Up Weight: {workout.warmUpWeight}</Text>
-            <Text style={styles.workoutText}>Final Weight: {workout.finalWeight}</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#0F172A' }}>
+      <ScrollView style={styles.container}>
+        <Text style={styles.header}>Workout Page</Text>
+        <Calendar
+          onDayPress={handleDateSelect}
+          markedDates={getMarkedDates()}
+          theme={{
+            calendarBackground: '#1E293B',
+            textSectionTitleColor: '#94A3B8',
+            selectedDayBackgroundColor: '#38BDF8',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: '#38BDF8',
+            dayTextColor: '#ffffff',
+            textDisabledColor: '#3B4252',
+            monthTextColor: '#ffffff',
+            arrowColor: '#38BDF8',
+            indicatorColor: '#38BDF8',
+          }}
+        />
+        <Button title="Add Workout" onPress={() => setShowForm(!showForm)} />
+        {showForm && (
+          <View style={styles.form}>
+            <Text style={styles.label}>Machine</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Machine"
+              placeholderTextColor="#94A3B8"
+              value={form.machine}
+              onChangeText={(value) => handleInputChange('machine', value)}
+            />
+            <Text style={styles.label}>Sets</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Sets"
+              placeholderTextColor="#94A3B8"
+              value={form.sets}
+              onChangeText={(value) => handleInputChange('sets', value)}
+              keyboardType="numeric"
+            />
+            <Text style={styles.label}>Reps</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Reps"
+              placeholderTextColor="#94A3B8"
+              value={form.reps}
+              onChangeText={(value) => handleInputChange('reps', value)}
+              keyboardType="numeric"
+            />
+            <Text style={styles.label}>Warm Up Weight (kg)</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Warm Up Weight"
+              placeholderTextColor="#94A3B8"
+              value={form.warmUpWeight}
+              onChangeText={(value) => handleInputChange('warmUpWeight', value)}
+              keyboardType="numeric"
+            />
+            <Text style={styles.label}>Final Weight (kg)</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Final Weight"
+              placeholderTextColor="#94A3B8"
+              value={form.finalWeight}
+              onChangeText={(value) => handleInputChange('finalWeight', value)}
+              keyboardType="numeric"
+            />
+            <Button title="Submit" onPress={handleSubmit} />
           </View>
-        ))}
-      </View>
-    </ScrollView>
+        )}
+        <View style={styles.workoutList}>
+          {workouts[selectedDate] && workouts[selectedDate].map((workout, index) => (
+            <View key={index} style={styles.workoutItem}>
+              <Text style={styles.workoutText}>Machine: {workout.machine}</Text>
+              <Text style={styles.workoutText}>Sets: {workout.sets}</Text>
+              <Text style={styles.workoutText}>Reps: {workout.reps}</Text>
+              <Text style={styles.workoutText}>Warm Up Weight: {workout.warmUpWeight} kg</Text>
+              <Text style={styles.workoutText}>Final Weight: {workout.finalWeight} kg</Text>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#0F172A',
+  },
   container: {
     flex: 1,
     backgroundColor: '#0F172A',
@@ -164,14 +165,19 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     marginBottom: 10,
+    height: 40,
+  },
+  label: {
+    color: '#94A3B8',
+    marginBottom: 5,
   },
   workoutList: {
     marginTop: 20,
   },
   workoutItem: {
     backgroundColor: '#1E293B',
-    padding: 15,
-    borderRadius: 10,
+    padding: 10,
+    borderRadius: 5,
     marginBottom: 10,
   },
   workoutText: {
